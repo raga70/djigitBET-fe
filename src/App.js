f1bd7bc3fdc9ab6a1dac061ecf362fd6570ef699
Radoslav Radev <raga70@raga70sRog.localdomain>
2022-09-09 16:59:17 +0200

    before eject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import { hot } from 'react-hot-loader/root';
 import './App.css';
+import MenuAppBar from "./components/Appbar";
+import ResponsiveAppBar from "./components/Appbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <ResponsiveAppBar/>
+        <h1>hello</h1>
     </div>
   );
 }
 
-export default App;
+export default hot(App);
